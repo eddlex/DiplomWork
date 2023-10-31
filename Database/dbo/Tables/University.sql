b0261b4fe0087f3ef6ac4796d9781d7fc2d1fef1
eddlex <eduardordukhanyan@gmail.com>
2023-10-31 18:04:46 +0400

    change university table name null -> not null

--- Database/dbo/Tables/University.sql
+++ Database/dbo/Tables/University.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[University] (
     [Id]          BIGINT         NOT NULL,
-    [Name]        NVARCHAR (50)  NULL,
+    [Name]        NVARCHAR (50)  NOT NULL,
     [Description] NVARCHAR (100) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     UNIQUE NONCLUSTERED ([Name] ASC)
 );
+
+
 
