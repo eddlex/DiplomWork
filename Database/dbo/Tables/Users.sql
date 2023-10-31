b687c153d93c0469aa1eea0f65c8d4c65f15273c
eddlex <eduardordukhanyan@gmail.com>
2023-10-31 13:14:09 +0400

    Addes Users && UsersInfo table

---
+++ Database/dbo/Tables/Users.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           INT            IDENTITY (0, 1) NOT NULL,
+    [Username]     VARCHAR (50)   NOT NULL,
+    [Email]        VARCHAR (100)  NOT NULL,
+    [Phone]        VARCHAR (100)  NOT NULL,
+    [Password]     NVARCHAR (500) NOT NULL,
+    [Salt]         NVARCHAR (50)  NOT NULL,
+    [CreationDate] DATETIME2 (7)  DEFAULT (getutcdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([Email] ASC),
+    UNIQUE NONCLUSTERED ([Phone] ASC),
+    UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
---
+++ Database/dbo/Tables/UsersInfo.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UsersInfo] (
+    [Id]              INT           IDENTITY (0, 1) NOT NULL,
+    [UserId]          INT           NULL,
+    [FirstName]       NVARCHAR (50) NULL,
+    [LastName]        NVARCHAR (50) NULL,
+    [BirthDate]       DATETIME2 (2) NULL,
+    [EmailIsVerified] BIT           DEFAULT ((0)) NULL,
+    [PhoneIsVerified] BIT           DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
+);
+
