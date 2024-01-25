0f9f6fa632ca8a31283f33409a890f401ba7a886
Irina Gevorgyan <Irina Gevorgyan@PC-1072>
2024-01-25 18:41:37 +0400

    Added front for users

---
+++ FrontEnd/Pages/Users.razor.cs
@@ -0,0 +1,22 @@
+﻿
+using FrontEnd.Interface;
+using FrontEnd.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+
+namespace FrontEnd.Pages;
+
+public class Element
+{
+    public int UserId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string UniversityId { get; set; }
+    public string Phone { get; set; }
+    public string Role { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
