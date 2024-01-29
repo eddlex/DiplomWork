04dd50a255fca6294d9df7720b9856d74f32ff91
Eduard Ordukhanyan <eduardordukhanyan@gmail.com>
2024-01-29 17:56:42 +0400

    code correction

--- FrontEnd/API/AuthorizationService.cs
+++ FrontEnd/API/AuthorizationService.cs
@@ -15,8 +15,9 @@ public class AuthorizationService :  HttpService, IAuthorizationService
     }
     
     public async Task<bool> AuthorizeClient(AuthorizationPost input)
-    {
-       var token = await Execute<string, AuthorizationPost>(HttpMethod.Post, "Security/Authorize", input);
+    { 
+        input.Password = input.Password.ComputeSHA512Hash();
+        var token = await Execute<string, AuthorizationPost>(HttpMethod.Post, "Security/Authorize", input);
       // var token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCIsImN0eSI6IkpXVCJ9.eyJVc2VySWQiOiIwIiwiVW5pdmVyc2l0eUlkIjoiMCIsInJvbGUiOiIwIiwianRpIjoiOGJjOTg3NzItZDM1OC00ODE1LWJjZmItNTFkZWNjYmMzYzFkIiwibmJmIjoxNzA2NDYyNzM0LCJleHAiOjE3MDY0NjYzMzQsImlhdCI6MTcwNjQ2MjczNCwiaXNzIjoibW9oYW1hZGxhd2FuZC5jb20iLCJhdWQiOiJtb2hhbWFkbGF3YW5kLmNvbSJ9.ENbYLl1eSufNCD0J1B81Vur-Devsn9wGOMY5T-FlaBhRTIPI9-rOSPlb-_Eaox9fJlwOLpCTEUaaXax8eDFA9w";
        
   
--- FrontEnd/Pages/Login.razor.cs
+++ FrontEnd/Pages/Login.razor.cs
@@ -1,59 +1,22 @@
-﻿
-using System.Runtime.InteropServices;
-using System.Security.Claims;
-using FrontEnd.API;
-using FrontEnd.Interface;
-using FrontEnd.Model;
+﻿using FrontEnd.Interface;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.JSInterop;
-using MudBlazor;
-
-
-
 
 namespace FrontEnd.Pages
 {
     public partial class Login
     {
         [Inject]
-        private IAuthorizationService Authorization { get; set; }
-    
-    
+        private IAuthorizationService? Authorization { get; set; }
+        
         [Inject]
-        private ISnackbar Snackbar { get; set; }
-    
-       
-    
+        private NavigationManager? NavigationManager { get; set; }
+        
         private async void Authorize()
         {
-            //throw new Exception("hello");
-            if (model.Password.Length < 5  || model.LogIn.Length < 5) 
-                return;
-
-            if (await this.Authorization.AuthorizeClient(model))
+            if (this.Authorization != null && await Authorization.AuthorizeClient(model))
             { 
-                
-              //  await AuthStateProvider.GetAuthenticationStateAsync();
-                navMeneger.NavigateTo("/AdminPage");
+                this.NavigationManager?.NavigateTo("/");
             }
-        
-    
-        
-        
-
-   
-
         }
-    
-    
-   
-
-        // protected override async Task OnInitializedAsync()
-        // {
-        //     await base.OnInitializedAsync();
-        // }
-        //
-
     }
 }
