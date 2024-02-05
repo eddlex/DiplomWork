03c1417bfd0eca3c09307a4d29b086a43d715c5f
Irina Gevorgyan <Irina Gevorgyan@PC-1072>
2024-02-05 17:47:36 +0400

    added Recipients

---
+++ FrontEnd/Model/Recipient.cs
@@ -0,0 +1,11 @@
+namespace FrontEnd.Model;
+
+public class Recipient
+{
+    public int Id { get; set; }
+    public int GroupId { get; set; }
+    public string Mail { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int UniversityId { get; set; }
+}
---
+++ FrontEnd/Pages/Recipients.razor.cs
@@ -0,0 +1,21 @@
+﻿
+using FrontEnd.Interface;
+using FrontEnd.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+
+namespace FrontEnd.Pages;
+
+public partial class Recipients
+{
+    //[Inject]
+    //private IUserService? UserService { get; set; } 
+    //private List<Recipient?> GridUsers { get; set;}
+
+    //protected override async Task OnInitializedAsync()
+    //{
+    //     this.GridUsers = await this.UserService.GetUsers();
+    //}
+    private List<Recipient> RecipientsGrid = new List<Recipient>();
+}
