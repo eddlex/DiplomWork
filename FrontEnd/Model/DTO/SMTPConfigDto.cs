3e335da01e245d4d4c3c04183608c6db185b68ed
Irina <gevorgyanirin070@gmail.com>
2024-02-22 00:33:47 +0400

    added smtp and changed select

---
+++ FrontEnd/Model/DTO/SMTPConfigDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using FrontEnd.Helpers;
+
+namespace FrontEnd.Model.DTO;
+
+public class SMTPConfigDto
+{ 
+    public int Id { get; set; }
+    public int DepartmentId { get; set; }
+    public string? SmtpServer { get; set; }
+    public int Port { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public bool EnableSSL { get; set; }
+
+
+}
--- FrontEnd/Pages/Smtp.razor.cs
+++ FrontEnd/Pages/Smtp.razor.cs
@@ -12,55 +12,90 @@ namespace FrontEnd.Pages;
 
 public partial class Smtp
 {
+
+
     [Inject]
-    private IDepartmentService? DepartmentService { get; set; } 
+    private ISmtpService? SmtpService { get; set; }
 
+    [Inject]
+    private IDepartmentService? DepartmentService { get; set; }
 
-    private List<RecipientDto?>? SmtpDto { get; set; }
-    private List<Recipient>? SmtpBl { get; set; }
+    private List<SMTPConfigDto?>? SMTPConfigDto { get; set; }
+    private List<SMTPConfig>? SMTPConfigBl { get; set; }
     private List<Department>? Departments { get; set; }
-   
-    
+
     protected override async Task OnInitializedAsync()
     {
-        if (this.DepartmentService != null)
+        if (this.SmtpService != null &&
+            this.DepartmentService != null)
         {
+            this.SMTPConfigBl = await this.SmtpService.GetSmtpConfigurations();
+            this.Departments = await this.DepartmentService.GetDepartments();
+            this.SMTPConfigDto = new();
+            if (this.SMTPConfigBl is { Count: > 0 })
+            {
 
-           this.Departments = await this.DepartmentService.GetDepartments();
-           this.SmtpDto = new();
-           
-           if (this.SmtpBl is { Count: > 0 })
-           {
-               
-               this.SmtpBl.ForEach(e => this.SmtpDto.Add(new RecipientDto()
-               {
-                   Id = e.Id,
-                   Name = e.Name,
-                   Description = e.Description,
-                   Department = this.Departments?.FirstOrDefault(d => d.Id == e.DepartmentId)?.Name,
-       
-                   Mail = e.Mail
-               }));
-           }
-           
-           
-           
-        } 
+                this.SMTPConfigBl.ForEach(e => this.SMTPConfigBl.Add(new SMTPConfigDto()
+                {
+                    Id = e.Id,
+                    DepartmentId = (int)this.Departments?.FirstOrDefault(d => d.Id == e.DepartmentId).Id,
+                    SmtpServer = e.SmtpServer,
+                    Port = e.Port,
+                    UserName = e.UserName,
+                    Password = e.Password,
+                    EnableSSL = e.EnableSSL
+                }));
+            }
+        }
     }
 
-    private async Task EditSmtp(int id)
+    private async Task EditSMTP(int id)
     {
-        
+        //if (this.RecipientService != null)
+        //{
+        //    var editedRowBl = this.RecipientBl?.Find(r => r.Id == id);
+        //    var editedRowDto = this.RecipientDto?.Find(r => r?.Id == id);
 
+
+
+        //    var recipient = await OpenDialog(editedRowBl);
+        //    if (recipient != default && editedRowBl is not null)
+        //    {
+        //        var result = await this.RecipientService.EditRecipient(recipient);
+        //        if (result != null)
+        //        {
+        //            this.RecipientDto?.Remove(editedRowDto);
+        //            this.RecipientBl?.Remove(editedRowBl);
+        //            this.RecipientBl?.Add(result);
+
+        //            this.RecipientDto?.Add(new RecipientDto()
+        //            {
+        //                Id = result.Id,
+        //                Name = result.Name,
+        //                Description = result.Description,
+        //                Department = this.Departments?.FirstOrDefault(d => d.Id == result.DepartmentId)?.Name,
+        //                Group = this.RecipientsGroups?.FirstOrDefault(d => d.Id == result.GroupId)?.Name,
+        //                Mail = result.Mail
+        //            });
+        //        }
+        //    }
+        //}
     }
-    
-    
-    private async Task DeleteSmtp(int id)
+
+
+    private async Task DeleteSMTP(int id)
     {
-        
+        //if (this.RecipientService != null && await ConfirmDelete())
+        //{
+        //    var result = await this.RecipientService.DelRecipient(RecipientBl.Find(r => r.Id == id));
+        //    if (result.HasValue)
+        //    {
+        //        RecipientDto?.Remove(RecipientDto.FirstOrDefault(row => row?.Id == result.Value));
+        //    }
+        //}
     }
-    
-    
+
+
     private async Task<bool> ConfirmDelete()
     {
         var parameters = new DialogParameters<DeleteDialog>();
@@ -70,72 +105,74 @@ public partial class Smtp
 
         var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
 
-        return !(await(await DialogService.ShowAsync<DeleteDialog>("Delete", parameters, options)).Result).Canceled;
-        
+        return !(await (await DialogService.ShowAsync<DeleteDialog>("Delete", parameters, options)).Result).Canceled;
+
     }
-    private async Task AddSmtp()
+    private async Task AddSMTP()
     {
-       // var recipient = await OpenDialog();
-       // if (recipient != default && this.RecipientService != null)
-       // {
-       //     var result = await this.RecipientService.AddRecipient(recipient);
-       //     if (result != null)
-       //     {
-       //         this.RecipientDto?.Add(new RecipientDto()
-       //         {
-       //             Id = result.Id,
-       //             Name = result.Name,
-       //             Description = result.Description,
-       //             Department = this.Departments?.FirstOrDefault(d => d.Id == result.DepartmentId)?.Name,
-       //             Group = this.RecipientsGroups?.FirstOrDefault(d => d.Id == result.GroupId)?.Name,
-       //             Mail = result.Mail
-       //         });
-       //     }
-       // }
+        //var recipient = await OpenDialog();
+        //if (recipient != default && this.RecipientService != null)
+        //{
+        //    var result = await this.RecipientService.AddRecipient(recipient);
+        //    if (result != null)
+        //    {
+        //        this.RecipientDto?.Add(new RecipientDto()
+        //        {
+        //            Id = result.Id,
+        //            Name = result.Name,
+        //            Description = result.Description,
+        //            Department = this.Departments?.FirstOrDefault(d => d.Id == result.DepartmentId)?.Name,
+        //            Group = this.RecipientsGroups?.FirstOrDefault(d => d.Id == result.GroupId)?.Name,
+        //            Mail = result.Mail
+        //        });
+        //    }
+        //}
     }
 
-    private async Task<Recipient?> OpenDialog(Recipient? row = default)
+    private async Task<SMTPConfig?> OpenDialog(SMTPConfig? row = default)
     {
-        var options = new DialogOptions 
-        {
-            CloseOnEscapeKey = true,
-            MaxWidth = MaxWidth.Large,
-            Position = DialogPosition.Center,
-        };
-        
-        var parameters = new DialogParameters<RecipientDialog>();
+        return new();
+        //var options = new DialogOptions
+        //{
+        //    CloseOnEscapeKey = true,
+        //    MaxWidth = MaxWidth.Large,
+        //    Position = DialogPosition.Center,
+        //};
 
-        
-        
-        var dialog = new RecipientDialog()
-        {
-            Department = new Select<Department>("Select Department", "DepartmentId", this.Departments),
-        };
+        //var parameters = new DialogParameters<RecipientDialog>();
 
-        if (row is not null)
-        {
-            dialog.Name = row.Name;
-            dialog.Mail = row.Mail;
-            dialog.Description = row.Description;
-            dialog.Department.SelectedValue = row.DepartmentId;
-            dialog.Group.SelectedValue = row.GroupId;
-        }
-         
-        parameters.Add("ObjectType", dialog);
-         
-        var result = await(await DialogService.ShowAsync<DialogComponent<RecipientDialog>>("Add Recipient", parameters, options)).Result;
-        if (!result.Canceled)
-        {
-            return new Recipient()
-            {
-                DepartmentId = dialog.Department.SelectedValue.Value,
-                GroupId = dialog.Group.SelectedValue.Value,
-                Mail = dialog.Mail,
-                Description = dialog.Description,
-                Name = dialog.Name
-            };
-        }
 
-        return default;
+
+        //var dialog = new RecipientDialog()
+        //{
+        //    Department = new Select<Department>("Select Department", "DepartmentId", this.Departments),
+        //    Group = new Select<RecipientGroup>("Select Group", "GroupId", this.RecipientsGroups)
+        //};
+
+        //if (row is not null)
+        //{
+        //    dialog.Name = row.Name;
+        //    dialog.Mail = row.Mail;
+        //    dialog.Description = row.Description;
+        //    dialog.Department.SelectedValue = row.DepartmentId;
+        //    dialog.Group.SelectedValue = row.GroupId;
+        //}
+
+        //parameters.Add("ObjectType", dialog);
+
+        //var result = await (await DialogService.ShowAsync<DialogComponent<RecipientDialog>>("Add Recipient", parameters, options)).Result;
+        //if (!result.Canceled)
+        //{
+        //    return new Recipient()
+        //    {
+        //        DepartmentId = dialog.Department.SelectedValue.Value,
+        //        GroupId = dialog.Group.SelectedValue.Value,
+        //        Mail = dialog.Mail,
+        //        Description = dialog.Description,
+        //        Name = dialog.Name
+        //    };
+        //}
+
+        //return default;
     }
 }
