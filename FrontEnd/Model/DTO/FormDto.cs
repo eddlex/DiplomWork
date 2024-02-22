93fd0789da97569eb5c560099edd05e2dc6ff2a9
Eduard Ordukhanyan <eduardordukhanyan@gmail.com>
2024-02-22 17:55:32 +0400

    add Form page

---
+++ FrontEnd/Model/DTO/FormDto.cs
@@ -0,0 +1,10 @@
+namespace FrontEnd.Model;
+
+public class FormDto
+{
+    public int Id { get; set; }
+    public int GroupId { get; set; }
+    public string? Name { get; set; }
+    public int DepartmentId { get; set; }
+    public string? Description { get; set; }
+}
