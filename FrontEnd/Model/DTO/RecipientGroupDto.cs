63b74b7315196c148721e1bd1e446ae14e6f605c
Irina <gevorgyanirin070@gmail.com>
2024-02-22 18:29:24 +0400

    added recipient group

---
+++ FrontEnd/Model/DTO/RecipientGroupDto.cs
@@ -0,0 +1,15 @@
+﻿using FrontEnd.Helpers;
+
+namespace FrontEnd.Model.DTO;
+
+public class RecipientGroupDto
+{
+    [CustomAttributes.EnumValue]
+    public int     Id           { get; set; }
+    
+    [CustomAttributes.EnumKey]
+    public string? Name         { get; set; }
+    
+    public string?   Department { get; set; }
+    public string? Description  { get; set; }
+}
---
+++ FrontEnd/Pages/RecipientGroups.razor.cs
@@ -0,0 +1,158 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Interface;
+using FrontEnd.Model;
+using FrontEnd.Model.BL;
+using FrontEnd.Model.Dialog;
+using FrontEnd.Model.DTO;
+using FrontEnd.Shared.Dialog;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace FrontEnd.Pages;
+
+public partial class RecipientGroups
+{
+    [Inject]
+    private IRecipientService? RecipientService { get; set; } 
+    
+    [Inject]
+    private IDepartmentService? DepartmentService { get; set; } 
+
+    private List<RecipientGroupDto?>? RecipientGroupDto { get; set; }
+    private List<RecipientGroup>? RecipientGroupBl { get; set; }
+    private List<Department>? Departments { get; set; }
+    //private List<RecipientGroup>? RecipientsGroups { get; set; }
+    
+    
+    protected override async Task OnInitializedAsync()
+    {
+        if (this.RecipientService != null && 
+            this.DepartmentService != null)
+        {
+           this.RecipientGroupBl = await this.RecipientService.GetRecipientsGroups();
+           //this.RecipientGroupDto = await this.RecipientService.GetRecipientsGroups();
+           this.Departments = await this.DepartmentService.GetDepartments();
+           this.RecipientGroupDto = new();
+           if (this.RecipientGroupBl is { Count: > 0 })
+           {
+               
+               this.RecipientGroupBl.ForEach(e => this.RecipientGroupDto.Add(new RecipientGroupDto()
+               {
+                   Id = e.Id,
+                   Name = e.Name,
+                   Description = e.Description,
+                   Department = this.Departments?.FirstOrDefault(d => d.Id == e.DepartmentId)?.Name              
+               }));
+           }                  
+        } 
+    }
+
+    private async Task EditRecipientGroup(int id)
+    {
+    //    if (this.RecipientService != null)
+    //    {
+    //        var editedRowBl = this.RecipientGroupBl?.Find(r => r.Id == id);
+    //        var editedRowDto = this.RecipientGroupDto?.Find(r => r?.Id == id);
+        
+            
+            
+    //        var recipient = await OpenDialog(editedRowBl);
+    //        if (recipient != default && editedRowBl is not null )
+    //        {
+    //            var result = await this.RecipientService.EditRecipient(recipient);
+    //            if (result != null)
+    //            {
+    //                this.RecipientGroupDto?.Remove(editedRowDto);
+    //                this.RecipientGroupBl?.Remove(editedRowBl);
+    //                this.RecipientGroupBl?.Add(result);
+                    
+    //                this.RecipientGroupDto?.Add(new RecipientGroupDto()
+    //                {
+    //                    Id = result.Id,
+    //                    Name = result.Name,
+    //                    Description = result.Description,
+    //                    Department = this.Departments?.FirstOrDefault(d => d.Id == result.DepartmentId)?.Name
+    //                });
+    //            }
+    //        }
+    //    }
+    }
+    
+    
+    private async Task DeleteRecipientGroup(int id)
+    {
+    //    if (this.RecipientService != null && await DialogService.DeleteConfirmationPopUp())
+    //    {
+    //        var result = await this.RecipientService.DelRecipient(RecipientGroupBl.Find(r => r.Id == id));
+    //        if (result.HasValue)
+    //        {
+    //            RecipientGroupDto?.Remove(RecipientGroupDto.FirstOrDefault(row => row?.Id == result.Value));
+    //        }
+    //    }
+    }
+    
+  
+    private async Task AddRecipientGroup()
+    {
+    //   var recipient = await OpenDialog();
+    //   if (recipient != default && this.RecipientService != null)
+    //   {
+    //       var result = await this.RecipientService.AddRecipient(recipient);
+    //       if (result != null)
+    //       {
+    //           this.RecipientGroupDto?.Add(new RecipientGroupDto()
+    //           {
+    //               Id = result.Id,
+    //               Name = result.Name,
+    //               Description = result.Description,
+    //               Department = this.Departments?.FirstOrDefault(d => d.Id == result.DepartmentId)?.Name
+    //           });
+    //       }
+    //   }
+    }
+
+    private async Task<RecipientGroup?> OpenDialog(RecipientGroup? row = default)
+    {
+    //    var options = new DialogOptions 
+    //    {
+    //        CloseOnEscapeKey = true,
+    //        MaxWidth = MaxWidth.Large,
+    //        Position = DialogPosition.Center,
+    //    };
+        
+    //    var parameters = new DialogParameters<RecipientDialog>();
+
+        
+        
+    //    var dialog = new RecipientDialog()
+    //    {
+    //        Department = new Select<Department>("Select Department", "DepartmentId", this.Departments),
+    //        //Group = new Select<RecipientGroup>("Select Group", "GroupId", this.RecipientsGroups)
+    //    };
+
+    //    if (row is not null)
+    //    {
+    //        dialog.Name = row.Name;
+    //        dialog.Description = row.Description;
+    //        dialog.Department.SelectedValue = row.DepartmentId;
+    //    }
+         
+    //    parameters.Add("ObjectType", dialog);
+         
+    //    var result = await(await DialogService.ShowAsync<DialogComponent<RecipientDialog>>("Add Recipient", parameters, options)).Result;
+    //    if (!result.Canceled && dialog.Department.SelectedValue != null && dialog.Group.SelectedValue != null)
+    //    {
+    //        return new Recipient()
+    //        {
+    //            DepartmentId = dialog.Department.SelectedValue.Value,
+    //            GroupId = dialog.Group.SelectedValue.Value,
+    //            Mail = dialog.Mail,
+    //            Description = dialog.Description,
+    //            Name = dialog.Name,
+    //            Id = row?.Id ?? 0
+    //        };
+    //    }
+
+        return default;
+    }
+}
