ca92d6e3c497a315a7898b9908110d1c253fe789
eduardordukhanyan <e.ordukhanyan@gmail.com>
2024-03-10 18:29:38 +0400

    change name table name and add spGetSubjects.sql script

---
+++ Database/dbo/Stored Procedures/spGetSubjects.sql
@@ -0,0 +1,13 @@
+﻿CREATE  PROCEDURE  spGetSubjects
+@RoleId INT,
+@DepartmentId INT
+AS
+BEGIN
+    SELECT Id
+          ,Title
+          ,Outcome
+          ,OutcomeTypeId
+          ,DepartmentId
+    FROM Subject
+    WHERE  @RoleId = 2 OR (@RoleId IN (0, 1) AND DepartmentId = @DepartmentId)
+END
--- Database/dbo/Tables/Subject.sql
+++ Database/dbo/Tables/Subject.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE Subjects(
+﻿CREATE TABLE Subject(
 	[Id] INT PRIMARY KEY IDENTITY(0,1),
 	[Title] NVARCHAR(200),
 	[Outcome] NVARCHAR(500),
