49cad23e9a70b7af47d81dfa4165dde768e9a6a3
eduardordukhanyan <e.ordukhanyan@gmail.com>
2024-03-24 20:06:37 +0400

    bug fix

--- Database/dbo/Stored Procedures/spAddRatings.sql
+++ Database/dbo/Stored Procedures/spAddRatings.sql
@@ -7,21 +7,15 @@ BEGIN
         BEGIN TRANSACTION
 
             DECLARE @CurrentDate DATETIME2(7) = GETUTCDATE()
+            
+            INSERT INTO Rating(FormIdentificationId, FormRowId , Value, LastUpdateDate, CreationDate)
+            SELECT @FormIdentificationId, r.FormRowId, r.Value, @CurrentDate, @CurrentDate
+            FROM @Ratings r where r.Id = -1
 
-            IF (SELECT ISNULL(COUNT(*), 0)
-                FROM @Ratings r
-                WHERE r.Id <> 0) =  0
-                BEGIN
-                    INSERT INTO Rating(FormIdentificationId, FormRowId , Value, LastUpdateDate, CreationDate)
-                    SELECT @FormIdentificationId, r.FormRowId, r.Value, @CurrentDate, @CurrentDate
-                    FROM @Ratings r where r.Id <> 0
-                END
-            ELSE
-                BEGIN
-                    UPDATE r
-                    SET r.Value = tmp.Value
-                    FROM Rating r JOIN @Ratings tmp ON r.Id = tmp.Id
-                END
+            UPDATE r
+            SET r.Value = tmp.Value
+            FROM Rating r JOIN @Ratings tmp ON r.Id = tmp.Id AND r.Id <> -1
+
         COMMIT
     END TRY
     BEGIN CATCH
