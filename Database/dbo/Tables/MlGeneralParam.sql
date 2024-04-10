cc840b3601d0263ebd98c8b17d6fa93a436da2a0
eduardordukhanyan <e.ordukhanyan@gmail.com>
2024-04-10 19:05:20 +0400

    tables for ml data

---
+++ Database/dbo/Tables/MlGeneralParam.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE MlGeneralParam
+(
+    Id              INT IDENTITY (0, 1) NOT NULL    PRIMARY KEY,
+    DepartmentId    INT                 NOT NULL    FOREIGN KEY REFERENCES Department(Id),
+    B               DECIMAL             NOT NULL,
+    LastUpdateDate  DATETIME2(7)        NOT NULL    DEFAULT GETUTCDATE(),
+    CreationDate    DATETIME2(7)        NOT NULL    DEFAULT GETUTCDATE()
+)
+
+GO
+CREATE UNIQUE INDEX  MlGeneralParam_DepartmentId ON MlGeneralParam(DepartmentId)
---
+++ Database/dbo/Tables/MlSubjectCalculation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE MlSubjectCalculation
+(
+    Id              INT IDENTITY (0, 1) NOT NULL    PRIMARY KEY,
+    SubjectId       INT                 NOT NULL    FOREIGN KEY REFERENCES Subject(Id),
+    Actual          DECIMAL             NOT NULL,
+    Predicted       DECIMAL             NOT NULL,
+    Action          NVARCHAR(200)       NOT NULL,
+    CreationDate    DATETIME2(7)        NOT NULL    DEFAULT GETUTCDATE()
+)
