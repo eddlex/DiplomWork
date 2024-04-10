e7b71f48aa79ada1905c848065f78ac1456d4fd4
eduardordukhanyan <e.ordukhanyan@gmail.com>
2024-04-10 23:13:52 +0400

    code correction

--- Database/dbo/Tables/MlParam.sql
+++ Database/dbo/Tables/MlParam.sql
@@ -1,11 +1,14 @@
-﻿CREATE TABLE MlGeneralParam
+﻿CREATE TABLE MlParam
 (
     Id              INT IDENTITY (0, 1) NOT NULL    PRIMARY KEY,
     DepartmentId    INT                 NOT NULL    FOREIGN KEY REFERENCES Department(Id),
     B               DECIMAL             NOT NULL,
+    K1              DECIMAL             NOT NULL,
+    K2              DECIMAL             NOT NULL,
+    K3              DECIMAL             NOT NULL,
     LastUpdateDate  DATETIME2(7)        NOT NULL    DEFAULT GETUTCDATE(),
     CreationDate    DATETIME2(7)        NOT NULL    DEFAULT GETUTCDATE()
 )
 
 GO
-CREATE UNIQUE INDEX  MlGeneralParam_DepartmentId ON MlGeneralParam(DepartmentId)
+CREATE UNIQUE INDEX  MlGeneralParam_DepartmentId ON MlParam(DepartmentId)
