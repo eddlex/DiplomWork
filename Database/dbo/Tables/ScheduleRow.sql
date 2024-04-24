8f360ef2d40204deb8b8c3c2ca6da91f0bdfe2af
eduardordukhanyan <e.ordukhanyan@gmail.com>
2024-04-24 23:48:21 +0400

    schedule db table add

---
+++ Database/dbo/Tables/ScheduleRow.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE ScheduleRow
+(
+    Id INT IDENTITY(0, 1) PRIMARY KEY  NOT NULl,
+    ScheduleId INT FOREIGN KEY REFERENCES Schedule(Id),
+    SubjectId INT FOREIGN KEY REFERENCES  Subject(Id),
+    CalculatedHours DECIMAL NULL,
+    CreationDate DATETIME2(7) DEFAULT  GETUTCDATE(),
+)
+
+GO 
+CREATE INDEX UNIQUE_INDEX_ScheduleRow__ScheduleId__SubjectId
+    ON ScheduleRow (ScheduleId, SubjectId)
+GO
