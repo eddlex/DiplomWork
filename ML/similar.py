a8843e10af8f9cb5f9377338d81ea8a4f339993b
Irina <gevorgyanirin070@gmail.com>
2024-05-03 20:16:19 +0400

    changes in code to find similar suggestios

--- ML/similar.py
+++ ML/similar.py
@@ -2,7 +2,6 @@ import sys
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pyodbc
-import sqlalchemy
 import json
 import platform
 
@@ -27,55 +26,62 @@ except FileNotFoundError:
 connection = pyodbc.connect(connection_string_odbc)
 cursor = connection.cursor()
 
+
 def get_suggestions():
-    query = "SELECT Value, Id, FormIdentificationId FROM Suggestion"
+    query = "SELECT Value, Id FROM Suggestion"
+    
     cursor.execute(query)
-
-    # sentences = [row[0] for row in cursor.fetchall()]
-    # sentences = [sentence.lower() for sentence in sentences]
-
     return cursor.fetchall()
 
 
-def get_similars(suggestions, threshold):
-    vectorizer = TfidfVectorizer()
+def get_similars(suggestions, threshold, suggestion_id=None):
     sentences = [row[0] for row in suggestions]
-    # Convert sentences into TF-IDF vectors
-    tfidf_matrix = vectorizer.fit_transform(sentences)
-    similarity_matrix = cosine_similarity(tfidf_matrix)
-
-    similar_sentences = {}
-
-    for i in range(len(sentences) - 1):
-        for j in range(i + 1, len(sentences)):
-            if similarity_matrix[i][j] > threshold:
-                id_i = suggestions[i][1]  
-                id_j = suggestions[j][1]  
-            
-                similar_sentences[id_i] = suggestions[i][2], suggestions[i][0]
-                similar_sentences[id_j] = suggestions[j][2], suggestions[j][0]
-
-    return similar_sentences
-
-
-if __name__ == '__main__':
-    # args = sys.argv[1:]
+    ids = [row[1] for row in suggestions]
     
-    # threshold = float(args[0])
-    # if(len(args) == 2):
-    #     suggestion_id = int(args[1])
+    # Convert sentences into TF-IDF vectors
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(sentences)
+    
+    similarity_matrix = cosine_similarity(tfidf_matrix)
+    
+    similar_sentences = []
+    
+    if suggestion_id:
+        index_of_given = ids.index(suggestion_id)
+        
+        for i in range(len(sentences)):
+            if i != index_of_given and similarity_matrix[index_of_given][i] > threshold:
+                similar_sentences.append(ids[i])
+
+    else:
+        for i in range(len(sentences) - 1):
+            for j in range(i + 1, len(sentences)):
+                if similarity_matrix[i][j] > threshold:
+                    similar_sentences.append(ids[i])
+                    similar_sentences.append(ids[j])
+    
+    return list(set(similar_sentences))
+
+
+# python "c:\\Users\\Irin\\Desktop\\diplom_ML\\similar.py" 0.5 3 
+# python "c:\\Users\\Irin\\Desktop\\diplom_ML\\similar.py" 0.5 
+# first argument is threshold for similarity and is required
+# second argument is id of suggestion, if provided will return the lsit of similars, otherwise returns all similar suggestions
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    
+    threshold = float(args[0])
+    suggestion_id = None
+    
+    if len(args) == 2:
+        suggestion_id = int(args[1])
     
     suggestions = get_suggestions()
-    # sentences = [row[0] for row in suggestions]
-    # sentences = [sentence.lower() for sentence in sentences]
-    similars = get_similars(suggestions, 0.5)
-
-    similars = {int(k): v for k, v in similars.items()}
+    similars = get_similars(suggestions, threshold, suggestion_id)
+    
     similars_json = json.dumps(similars)
-
+    
     cursor.close()
     connection.close()
-
+    
     print(similars_json, end='')
-
-
